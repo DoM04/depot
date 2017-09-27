96c475076a2458b5975098295bde64caf7e8e7b1
Donal O'Malley <donalom1@gmail.com>
2017-09-27 13:24:44 +0100

    fixed broken tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	validates :title, :description, presence: true
+	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :image_url, allow_blank: true, format: {
