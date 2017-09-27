5f0afb356b970840e8db2e0ab917e20d530ea111
Donal O'Malley <donalom1@gmail.com>
2017-09-27 13:48:59 +0100

    fixed validator

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,6 @@ class Product < ActiveRecord::Base
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :image_url, allow_blank: true, format: {
   with: %r{\.(gif|jpg|png|jpeg)\Z}i,
-  message: 'must be a URL for GIF, JPG or PNG image.'
+  message: 'must be a URL for GIF, JPG,JPEG or PNG image.'
 }
 end
